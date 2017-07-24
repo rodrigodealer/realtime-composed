b232e2dc1fef8d036a86bc4c06205eff235baaef
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2017-07-24 14:56:24 +0200

    Log

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	fmt.Print("Starting server on port 8080")
 	err := http.ListenAndServe(":8080", server.Server())
 	if err != nil {
 		fmt.Println("Something is wrong : " + err.Error())
